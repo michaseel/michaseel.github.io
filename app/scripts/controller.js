4686fff45f11d6f1d10ffd7673d144da0d10f4b7
michaseel <seelmichael@gmail.com>
2014-04-01 17:42:34 +0200

    blurheader

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -7,30 +7,67 @@ var   NXB = {};
 /*** Detection ***/
 //TBD: Viewport Width, Touch...
 
-
 /******* Global Event Handlers *********/
 $(document).ready(function () {
   // Fired when DOM is ready = before Page is loaded
-  NXB.exampleFunction();
+
 });
+
 
 $(window).load(function () {
   // Fired when Page is loaded
+  frost();
 });
 
 $( window ).resize(function() {
   // Fired when window is resized
+  frost();
 });
 
+
+
+
+//var lastScrollTop = 0;
 $(window).scroll(function(){
+
+
+ $("canvas").css("-webkit-transform", "translatey(-" + $(window).scrollTop() + "px)");
+
+
+
+
   // Fired when window is scrolled
+//frost();
+  /*var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       $('header').addClass('header-small');
+   } else {
+      $('header').removeClass('header-small');
+   }
+   lastScrollTop = st;
+*/
 })
 
 
+frost = function () {
+        $(".blurheader").empty().css('height', $('header').height());
 
+        var w = $('#content').width();
+        html2canvas($('#content'), {
+            onrendered: function (canvas) {
+                $(".blurheader").append(canvas);
+                 $(".blurheader canvas").attr("id","canvas");
+            }
+        });
+        //$('canvas, #header, #cover').hide();
+        /*$('#cover').fadeIn('slow', function () {
+            $('#header').fadeIn('slow');
+        });*/
+    };
 
 /********** Global Functions ***********/
-NXB.exampleFunction = function() {
-  // Just an example...
-  return;
-};
+
+$(document).ready(function () {
+    $('body').append('<div id="contain"><canvas /></div>');
+    $('body').append('<div id="cover"></div><svg id="svg-image-blur"><filter id="blur-effect-1"><feGaussianBlur stdDeviation="2"/></filter></svg>');
+});
